3c6d000d4f75629d1144c0634be54dd5422e7e7d
Афлятунов Марсель (Aflyatunov_MR) <Aflyatunov_MR@directum.ru>
2022-03-17 12:43:26 +0500

    Add project files.

---
+++ ConsoleApp3/AssemblyWrapper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+  internal class AssemblyWrapper
+  {
+    #region Поля и свойства
+    private readonly Type type;
+    
+    /// <summary>
+    /// Объект типа класса
+    /// </summary>
+    public Type Type => type;
+
+    private readonly object instance;
+    
+    /// <summary>
+    /// Инстанс класса
+    /// </summary>
+    public object Instance => instance;
+    #endregion
+
+    #region Методы
+    /// <summary>
+    /// Испольняет статический метод класса
+    /// </summary>
+    /// <param name="methodName">Имя метода</param>
+    /// <param name="args">Аргументы</param>
+    /// <param name="result">Возрващаемое значение</param>
+    public void RunStatic(string methodName, object[] args, out object result)
+    {
+      if (string.IsNullOrEmpty(methodName))
+        throw new ArgumentNullException(nameof(methodName));
+
+      var method = this.type.GetMethods(BindingFlags.Public | BindingFlags.Static)
+        .FirstOrDefault(m => m.Name == methodName && m.GetParameters().Length == args.Length);
+      if (method == null)
+        throw new Exception($"I don't find {method} method");
+
+      result = method.Invoke(null, args);
+    }
+
+    /// <summary>
+    /// Запускает не статически метод класса
+    /// Необходимо наличие инстанса класса
+    /// </summary>
+    /// <param name="methodName">Имя метода</param>
+    /// <param name="args">Аргументы</param>
+    /// <param name="result">Возрващаемое значение</param>
+    public void Run(string methodName, object[] args, out object result)
+    {
+      if (string.IsNullOrEmpty(methodName))
+        throw new ArgumentNullException(nameof(methodName));
+
+      if (this.instance == null) 
+        throw new Exception("This is static class");
+
+      var method = this.type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+        .FirstOrDefault(m => m.Name == methodName && m.GetParameters().Length == args.Length);
+      if (method == null) new Exception($"I don't find {method} method");
+
+      result = method.Invoke(this.instance, args);
+    }
+    #endregion
+
+    #region Конструкторы
+    public AssemblyWrapper(string assemblyString, string typeName, bool isStatic)
+    {
+      var asm = Assembly.Load(assemblyString);
+      if (asm == null) throw new Exception("I don't load assembly");
+
+      this.type = asm.GetType(typeName);
+      if (this.type == null) 
+        throw new Exception($"I don't load {assemblyString} type from {typeName} assembly");
+
+      if (!isStatic) 
+      { 
+        this.instance = Activator.CreateInstance(this.type);
+        if (this.instance == null) 
+          throw new Exception($"I don't create instance of {typeName}");
+      }
+    }
+    #endregion
+  }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ConsoleApp3
+{
+  class Program
+  {
+    private const string AssemblyString = "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+    
+    private const string TypeName = "System.Windows.Forms.MessageBox";
+
+    private const string MethodName = "Show";
+
+    private const string FileName = "fields.txt";
+
+    static void Main(string[] args)
+    {
+      var messageBox = new AssemblyWrapper(AssemblyString, TypeName, true);
+      messageBox.RunStatic(MethodName, new[] { "Hellow World!" }, out object result);
+      
+      WriteToFile(messageBox.Type);
+    }
+
+    static void WriteToFile(Type type)
+    {
+      using(var stream = new FileStream(FileName, FileMode.Create))
+      using (var file = new StreamWriter(stream))
+      {
+        var methodNames = new StringBuilder();
+        foreach(var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance))
+        {
+          methodNames.Append(method.Name);
+          methodNames.AppendLine();
+        }
+
+        file.Write(methodNames);
+        file.Flush();
+      }
+    }
+  }
+}
